c1491f6be3f6854d2ec08322d0cd1fb76f9f6f02
Gregor Woiwode <gregor.woiwode@gmail.com>
2015-04-08 20:12:52 +0200

    Creating less-task

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var gulp = require('gulp');
+var less = require('gulp-less');
+
+
+gulp.task('less', function() {
+  return gulp.src('./styles/*.less')
+             .pipe(less())
+             .pipe(gulp.dest('./build-dev/css/'));
+});
