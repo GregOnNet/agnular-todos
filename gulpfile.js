e80b02b9ecfb7a9730ac4e076d3f5491a028b4eb
Gregor Woiwode <gregor.woiwode@gmail.com>
2015-04-08 22:37:40 +0200

    Decoupling js and less task from inject-task Renaming less-files to .css-files

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,12 @@
 
 var gulp            = require('gulp');
 var watch           = require('gulp-watch');
+var rename          = require('gulp-rename');
 var less            = require('gulp-less');
 var bower           = require('main-bower-files');
 var inject          = require('gulp-inject');
 var angularFilesort = require('gulp-angular-filesort');
 var browserSync     = require('browser-sync');
-
-var streams = {
-  'app'    : null,
-  'styles' : null
-};
 
 gulp.task('less-watch', ['less', 'inject'], browserSync.reload);
 gulp.task('js-watch', ['js', 'inject'], browserSync.reload);
@@ -22,32 +18,34 @@ gulp.task('watch', function() {
 });
 
 gulp.task('less', function() {
-  streams.styles = gulp.src('./styles/*.less')
-                       .pipe(less())
-                       .pipe(gulp.dest('./build-dev/css/'));
+  gulp.src('./src/client/content/*.less')
+      .pipe(less())
+      .pipe(gulp.dest('./build-dev/client/content/'));
 });
 
 gulp.task('js', function() {
-  streams.app = gulp.src('./src/client/app/**/*.js')
-                    .pipe(angularFilesort())
-                    .pipe(gulp.dest('./build-dev/client/app/'));
+  gulp.src('./src/client/app/**/*.js')
+      .pipe(angularFilesort())
+      .pipe(gulp.dest('./build-dev/client/app/'));
 });
 
 gulp.task('inject', function() {
-  var index = gulp.src('./src/client/index.html');
+  var index      = gulp.src('./src/client/index.html');
+  var appFiles   = gulp.src('./src/client/app/**/*.js').pipe(angularFilesort());
+  var appStyles  = gulp.src('./src/client/content/*.less').pipe(rename({ extname: '.css'}));
   var bowerFiles = gulp.src(bower(), { read: false });
 
   index
-    .pipe(inject(streams.app,    { name: 'app', ignorePath : 'build-dev' }))
-    .pipe(inject(streams.styles, { name: 'app', ignorePath : 'build-dev' }))
+    .pipe(inject(appFiles,  { name: 'app', relative: true }))
+    .pipe(inject(appStyles, { name: 'app', relative: true }))
     .pipe(inject(bowerFiles, { name: 'bower' }))
-    .pipe(gulp.dest('./build-dev/'));
+    .pipe(gulp.dest('./build-dev/client/'));
 });
 
 gulp.task('serve-dev', ['less', 'js', 'inject', 'watch'], function() {
   browserSync({
     server: {
-      baseDir: "./build-dev/",
+      baseDir: "./build-dev/client/",
       routes: {
           '/bower_components': 'bower_components'
       }
