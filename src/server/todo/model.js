82bbe5be499368bd2b6e96ad65b440447abb9b22
Gregor Woiwode <gregor.woiwode@gmail.com>
2015-05-06 09:31:15 +0200

    Using node-uuid generating id for a todo

--- src/server/todo/model.js
+++ src/server/todo/model.js
@@ -1,6 +1,9 @@
 'use strict';
 
-module.exports = function(title, task, category){
+var uuid = require('node-uuid');
+
+module.exports = function(title, task, category) {
+	this.id       = uuid.v1();
 	this.title    = title;
   this.task     = task;
   this.category = category;
